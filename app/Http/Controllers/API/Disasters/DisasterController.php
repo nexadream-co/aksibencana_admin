ca0c89fb9269831874f54ea1bbd51349fa0e04dd
mambaur <alwiroziq1998@gmail.com>
2024-12-29 14:58:01 +0700

    fix: disaster must has value

--- app/Http/Controllers/API/Disasters/DisasterController.php
+++ app/Http/Controllers/API/Disasters/DisasterController.php
@@ -20,7 +20,7 @@ class DisasterController extends Controller
             'category_id' => ['integer'],
         ]);
 
-        $disasters = Disaster::whereHas('disaster')->where('status', 'active');
+        $disasters = Disaster::where('status', 'active');
 
         if ($request->category_id) {
             $disasters = $disasters->where('disaster_category_id', $request->category_id);
@@ -73,7 +73,7 @@ class DisasterController extends Controller
             'category_id' => ['integer'],
         ]);
 
-        $disasters = Disaster::whereHas('disaster');
+        $disasters = Disaster::query();
 
         if ($request->category_id) {
             $disasters = $disasters->where('disaster_category_id', $request->category_id);
