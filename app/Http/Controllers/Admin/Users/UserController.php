771aa25e0c726aff3131b8dce7c088b188c09bb3
Mambaur Roziq Alwi <alwiroziq1998@gmail.com>
2025-06-22 14:55:05 +0700

    fix: deleted user in admin must set random email

--- app/Http/Controllers/Admin/Users/UserController.php
+++ app/Http/Controllers/Admin/Users/UserController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
